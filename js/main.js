c1f7ed2e8d224b57c78c97d97670e3ee758ec3ce
Luca Illica Magrini <86322780+LUCAIM08@users.noreply.github.com>
2025-05-20 15:25:41 +0200

    + | Added files

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function InvioModulo() {
+    let username = document.getElementById("username").value
+    let password = document.getElementById("password").value
+
+    alert("Loggato come: " + username + " | Password: " + password)
+}
